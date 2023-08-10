9a533cc09243383fe82804f9ab3b256888638559
ahmed025566 <ahmedsalah025566@gmail.com>
2023-08-10 03:34:01 +0300

    Add a method belongs to in student

--- student.rb
+++ student.rb
@@ -1,17 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classrrom
+  attr_reader :classrrom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 
-  def add_student
-    classroom.add_student(self) unless classroom.students.include?(self)
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
